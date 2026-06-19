f539e8ba1b8bd9ad4562496853dcfd3af7df3d85
Ryan B <beryshadow@gmail.com>
2026-06-19 13:24:29 -0400

    trying to fix time

--- portfolio/src/vite.config.js
+++ portfolio/src/vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   define: {
-    __BUILD_TIME__: JSON.stringify(new Date().toLocaleString())
+    // Vite natively supports extending import.meta.env
+    'import.meta.env.VITE_BUILD_TIME': JSON.stringify(new Date().toLocaleString())
   }
 });
