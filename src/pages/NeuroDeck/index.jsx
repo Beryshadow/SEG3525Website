866d40e4fa4d749e32c97dc0ee8124f7254209e0
Beryshadow <beryshadow@gmail.com>
2026-07-20 03:47:26 -0400

    fix: import useMemo

--- src/pages/NeuroDeck/index.jsx
+++ src/pages/NeuroDeck/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback, useRef } from 'react';
+import React, { useState, useEffect, useCallback, useRef, useMemo } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useSharedLogic } from '../../utilities/shared';
 import { useNLIModel } from '../../hooks/useNLIModel';
